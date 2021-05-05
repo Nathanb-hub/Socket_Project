193dc551ce9c747392e305ba6f30ab20b094402c
Nathanb-hub <nath.buchin@gmail.com>
2021-05-05 14:47:03 +0200

    Initial commit

---
+++ client.py
@@ -0,0 +1,72 @@
+import socket 
+import sys
+
+
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 5566 # localhost est l'adresse du serveur local equivalent a l'ip 127.0.0.1
+
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+running = True
+
+class Chat:
+    def __init__(self):
+        client.connect((HOST,PORT)) # pour connecter le client au serveur
+        self.conn = ((HOST,PORT)) 
+        print("Client connecté !")
+
+        
+
+    def _receive(self):
+        # pour recevoir des données
+        data = client.recv(1024) # taille de reception de données 
+        data = data.decode("utf-8")
+        if len(data)>0:
+            print("Reponse du serveur :")
+            print(data)
+            running = False
+
+    
+    def _send(self,msg):
+        data = msg.encode("utf-8")
+        client.sendall(data)
+        print(data)
+
+    def _connectedPeople(self):
+        request = "c".encode("utf8")
+        client.sendall(request)
+        self._receive()
+
+
+
+try:
+    chat = Chat()
+    try:
+        cmd = sys.argv[1]
+    except IndexError:
+        cmd = '_receive'
+    try:
+        param = sys.argv[2]
+    except IndexError:
+        param = ''
+    
+    if len(sys.argv)>3:
+        e = 3 
+        while e < len(sys.argv):
+            param = param + ' ' + sys.argv[e]
+            e+=1
+
+    handlers = {'send':chat._send,'receive':chat._receive,'connected':chat._connectedPeople}
+    if cmd in handlers:
+        handlers[cmd]() if param == '' else handlers[cmd](param)
+
+
+    # recevoir des données
+
+    while running:
+        chat._receive()
+
+except Exception as e:
+    print("Connexion échouée", e)
+
+finally:
+    client.close()
---
+++ server.py
@@ -0,0 +1,58 @@
+import socket
+import threading
+
+
+
+SERVER = socket.gethostbyname(socket.gethostname())
+PORT = 5566 # le serveur n'a pas besoin d'adresse car il ne fait qu'ecouter
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind((SERVER, PORT))
+print("Le serveur est démarré ... ")
+
+
+# pour gerer plusieur connexion de client simultanément 
+class ClientsHandling(threading.Thread): 
+    # elle hérite de thread
+    def __init__(self,conn,addr,clients = {}):
+        threading.Thread.__init__(self)
+        self.conn = conn 
+        self.clients = clients
+        self.host = addr[0]
+        self.port = server.getsockname()[1]
+    
+    def run(self):
+        data = self.conn.recv(1024) # taille de reception de données 
+        data = data.decode("utf-8")
+        print(data)
+        self.clients[(self.host,self.port)] = data
+        print(self.clients)
+        if data =='c':
+            #server.connect((self.host,self.port))
+            msg = ""
+            for client in self.clients:
+                print('client', client)
+                msg += client[0]
+            self.conn.sendall(msg.encode("utf8"))
+
+            
+
+
+        
+
+
+
+# boucle infinie pour que le serveur ecoute tant qu'une machine est connectée
+while True:
+    server.listen(5) # le parametre est le nombre de connexion qui peuvent échouer avant de refuser d'autres connexions
+    conn, addr = server.accept() # on stocke les info de la machine qui est actuellement connectée au serveur adress contient ip et le port 
+    print(f"Un client de la connexion {addr[0]} vient de se connecter sur le port {server.getsockname()[1]}")
+
+
+    my_thread = ClientsHandling(conn,addr)
+    my_thread.start() # appelle la méthode run de la classe ClientsHandling
+   
+
+
+conn.close()
+server.close()
